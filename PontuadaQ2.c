b280b5f4b6b855d5585d127629c506917c8769aa
kauansandes <144127535+kauansandes@users.noreply.github.com>
2023-11-28 13:21:47 -0300

    Questão 02 Atividade pontuada 03.c

--- PontuadaQ2.c
+++ PontuadaQ2.c
@@ -15,14 +15,14 @@ int main()
 
     for (i = 0; i < 5; i++)
     {
-        printf("Digite o nome do %d° aluno: ", i + 1);
+        printf("Digite o nome do %dÂ° aluno: ", i + 1);
         gets(nome[i]);
-        printf("Digite a idade do %d° aluno: ", i + 1);
+        printf("Digite a idade do %dÂ° aluno: ", i + 1);
         scanf("%d", &idade[i]);
 
         for (j = 0; j < 3; j++)
         {
-            printf("Digite a %d° nota do aluno: ", j + 1);
+            printf("Digite a %dÂ° nota do aluno: ", j + 1);
             scanf("%f", &notas[i][j]);
 
             somaNotas += notas[i][j];
@@ -32,7 +32,7 @@ int main()
         somaNotas = 0;
 
         fflush(stdin);
-        system ("cls||clear");
+        system("cls||clear");
     }
 
     for (i = 0; i < 5; i++)
@@ -45,20 +45,20 @@ int main()
             printf("Notas do aluno: %.2f \n", notas[i][j]);
         }
 
-        printf("Média do aluno: %.2f \n", media[i]);
+        printf("MÃ©dia do aluno: %.2f \n", media[i]);
 
         if (media[i] >= 7)
         {
             printf("Aprovado! \n");
         }
 
-        if (media[i] > 5 && media [i] < 7)
+        if (media[i] > 5 && media[i] < 7)
         {
-            printf("Recuperação. \n");
+            printf("RecuperaÃ§Ã£o. \n");
         }
-        if (media [i] < 5)
+        if (media[i] < 5)
         {
-    
+
             printf("Reprovado. \n");
         }
     }
