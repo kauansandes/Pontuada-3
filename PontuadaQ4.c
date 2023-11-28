8c137364e93c897928c919239692e53eeeb72b76
kauansandes <144127535+kauansandes@users.noreply.github.com>
2023-11-28 13:25:42 -0300

    Questão 04 Atividade Pontuada 03

--- PontuadaQ4.c
+++ PontuadaQ4.c
@@ -2,58 +2,53 @@
 #include <stdlib.h>
 #include <locale.h>
 
-int main() {
+int main()
+{
     setlocale(LC_ALL, "portuguese");
 
-    int numero[5];
-    int i;
-    int impar=0;
-    int par=0;
-    int qntNegativos=0;
-    int maior=0;
-    int menor=9999;
-    int somaPar=0;
-    int somaNumeros=0;
-    float mediaPar=0;
-    float mediaNumeros=0;
+    int numero[5], i, impar = 0, par = 0, qntNegativos = 0, maior = 0, menor = 9999, somaPar = 0, somaNumeros = 0;
+    float mediaPar = 0, mediaNumeros = 0;
 
-    for (i = 0; i < 5; i++) {
-        printf("Digite o %dº número: ", i + 1);
+    for (i = 0; i < 5; i++)
+    {
+        printf("Digite o %dº numero: ", i + 1);
         scanf("%d", &numero[i]);
-        system ("cls || clear");
+        system("cls || clear");
 
-        if (numero[i] % 2 == 0) {
+        if (numero[i] % 2 == 0)
+        {
             par++;
-            somaPar+=numero[i];
-        }else{
+            somaPar += numero[i];
+        }
+        else
+        {
             impar++;
-        } 
+        }
 
-        if (numero[i] < 0) {
+        if (numero[i] < 0)
+        {
             qntNegativos++;
         }
-        
-        somaNumeros+=numero[i];
-        
+
+        somaNumeros += numero[i];
+
         maior = numero[i] > maior ? numero[i] : maior;
         menor = numero[i] < menor ? numero[i] : menor;
-
     }
 
-        mediaNumeros = somaNumeros / i;
-            
-        if (par > 0) {
-        mediaPar = somaPar /  par; 
-       }
-        
-        printf("Quantidade de números ímpares: %d \n", impar);
-        printf("Quantidade de números negativos: %d \n", qntNegativos);
-        printf("Maior número: %d \n", maior);
-        printf("Menor número: %d \n", maior);
-        printf("Média números pares: %.1f \n", mediaPar);
-        printf("Média números total: %.1f \n", mediaNumeros);
+    mediaNumeros = somaNumeros / i;
 
+    if (par > 0)
+    {
+        mediaPar = somaPar / par;
+    }
+
+    printf("Quantidade de numeros i­mpares: %d \n", impar);
+    printf("Quantidade de numeros negativos: %d \n", qntNegativos);
+    printf("Maior numero: %d \n", maior);
+    printf("Menor numero: %d \n", maior);
+    printf("Média numeros pares: %.1f \n", mediaPar);
+    printf("Média numeros total: %.1f \n", mediaNumeros);
 
     return 0;
-
 }
